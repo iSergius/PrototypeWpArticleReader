022af1b566499f4f785273c3dc56fe33e74366e8
Kondratyev Sergey <ron.rcf@gmail.com>
2017-08-05 18:55:58 +0500

    Create entity class Article

---
+++ app/src/main/java/name/isergius/freelance/protowpreader/Article.java
@@ -0,0 +1,30 @@
+package name.isergius.freelance.protowpreader;
+
+/**
+ * @author Sergey Kondratyev
+ */
+
+public class Article {
+
+    private long id;
+    private String title;
+    private String content;
+
+    public Article(long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
